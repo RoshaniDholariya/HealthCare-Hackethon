4d7526d2b1610e1c48b40778a6fa300a934804f3
RoshaniDholariya <roshnidholariya@gmail.com>
2024-12-22 15:23:45 +0530

    signup login page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import SignIn from "./Authentication/SignIn";
+import Signup from "./Authentication/SignUp";
+
+
+function App() {
+
+  return (
+    <>
+     <SignIn/>
+     <Signup/>
+    </>
+  )
+}
+
+export default App
